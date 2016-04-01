8f49c58cedc42dbb92fdfff4bbe5d24c99fef519
radlee <lolito@projectcodex.co>
2016-04-01 12:02:41 +0200

    List Of Deals Separately

--- avo.js
+++ avo.js
@@ -3,18 +3,31 @@ var deal = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
 
 function splitString(stringToSplit, separator){
 	var arrayOfStrings = stringToSplit.split(separator);
-	var string = arrayOfStrings.join('');
-	// console.log(string);
-	return string;
+	// var string = arrayOfStrings.join(' ');
+	// console.log(arrayOfStrings);
+	return arrayOfStrings;
 }
 
-var x = splitString(deal, 'for R').split(',');
-
-console.log(x);
 
 
 var space = ' ';
 var comma = ',';
 var define=null;
 
-splitString(deal, 'for R')
+var x = splitString(deal, comma)
+
+
+var a = [];
+for (var i =0; i < x.length; i++){
+	var n = x[i].split(' for R')
+	a.push(n);
+}
+
+a.forEach(function(item){
+	console.log(item)
+})
+console.log(a.length);
+
+
+
+// splitString(splitString(deal, comma));
