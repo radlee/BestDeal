63b5bd9c654c38a71f1f30764c9e4fa0347e5e95
radlee <lolito@projectcodex.co>
2016-04-01 10:43:47 +0200

    Change an array to an Object

--- avo.js
+++ avo.js
@@ -1,47 +1,20 @@
-var deal = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
-
-var array = deal.split("for R");
+var deal = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
 
 
-	// var obj = {},
+function splitString(stringToSplit, separator){
+	var arrayOfStrings = stringToSplit.split(separator);
+	var string = arrayOfStrings.join('');
+	// console.log(string);
+	return string;
+}
 
-	// array = array, 
-	// l = array.length;
-	// while(l && (obj[--l] = array.pop())){
-	// };
+var x = splitString(deal, 'for R').split(',');
 
-	var arrObj = {};
-
-	for(var i=0; i< array.length; i++){
-		arrObj[array[i]] = i;
-	
-	}
+console.log(x);
 
 
+var space = ' ';
+var comma = ',';
+var define=null;
 
-
-
-
-console.log(arrObj.length);
-
-
-
-
-
-
-
-
-
-//////////////
-// var nums = {};
-
-// array.forEach(function(array){
-// 	if(nums[array] == undefined){
-// 		nums[array] = 0;
-// 	}
-
-
-// 	nums[array] = nums[array] + 1;
-// });
-
-// console.log((nums));
+splitString(deal, 'for R')
