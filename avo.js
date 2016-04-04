6a686c8c8a6502ecc951b1bdaa368286f104f094
lee <lee@lee-Inspiron-M5040.(none)>
2016-04-04 10:13:52 +0200

    Best Deal - Completed

--- avo.js
+++ avo.js
@@ -3,39 +3,45 @@ var deal = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
 
 function splitString(stringToSplit, separator){
 	var arrayOfStrings = stringToSplit.split(separator);
-	// var string = arrayOfStrings.join(' ');
-	// console.log(arrayOfStrings);
 	return arrayOfStrings;
 }
 
-
-
-var space = ' ';
-var comma = ',';
-var define=null;
-
-var x = splitString(deal, comma)
-
-
+var x = splitString(deal, ',')
+//Removing 'for R' from each
 var a = [];
 for (var i =0; i < x.length; i++){
 	var n = x[i].split(' for R')
 	a.push(n);
 }
 
+//Pushing prices for each deal to an array
+var prices =[];
 a.forEach(function(item){
 	var qua = item[0];
 	var price = item[1];
-
-	var ans = price/ qua;
-
-	console.log(ans)
-
-	// console.log(qua)
-	// console.log(price)
+	var ans = Number((price/ qua).toFixed(2));
+	prices.push(ans)
 })
-console.log(a.length);
 
+// Find out about Accessing the last item in the list 
+var sorted = prices.sort();
 
+console.log("\n---------------------");
+console.log(sorted)
+console.log("---------------------\n");
 
-// splitString(splitString(deal, comma));
+console.log("----------------");
+console.log("Cheap : " + sorted[0]) ;
+console.log("Expensive : " + sorted[sorted.length - 1])
+console.log("----------------\n");
+
+//Sum all numbers in the array
+var sum = 0;
+sorted.forEach(function(item){
+	sum += item;
+})
+//Get the average of the prices
+var average = sum / prices.length;
+console.log("----------------------------");
+console.log("The average price is :" + average.toFixed(2) + " |");
+console.log("----------------------------\n");
